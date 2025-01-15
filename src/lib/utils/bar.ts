fa812bec7f6900fbb401010d2c0997b4e1cf16c8
gantrol <501963347@qq.com>
2025-01-15 16:59:38 +0800

    modified: 编辑路由取消，侧边栏与nav都改为指向/edit/puzzles/1

--- src/lib/utils/bar.ts
+++ src/lib/utils/bar.ts
@@ -3,7 +3,7 @@ import {Circle, Dices, Inbox} from "lucide-svelte";
 export const commonItems = [
     { title: "随机", url: "/random", icon: Circle }, // TODO: 随机加载，并可以点击“下一题”
     { title: "题库", url: "/puzzles", icon: Inbox },
-    { title: "编题", url: "/edit", icon: Dices },
+    { title: "编题", url: "/edit/puzzles/1", icon: Dices },
 ];
 
 export const WUTHERING_WAVES = "鸣潮"
--- src/routes/edit/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Solver from '$lib/components/Solver.svelte';
-</script>
-
-<Solver
-        editMode={true}
-/>
