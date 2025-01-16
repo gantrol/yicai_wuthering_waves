2aa0462746697ecc3335c59d9f223f24a2990d64
gantrol <501963347@qq.com>
2025-01-16 15:06:06 +0800

    ui: 优化侧边栏显示

--- src/lib/components/AppSidebar.svelte
+++ src/lib/components/AppSidebar.svelte
@@ -62,45 +62,45 @@
         }
     }
 </script>
-{#if loadingSidebar}
-    <LoadingSpinner text="尽力拉取题目列表..."/>
-{:else}
-    <Sidebar.Root side="left" variant="sidebar" collapsible="offcanvas" class="border-r">
-        <Sidebar.Header class="px-4 py-2">
-            <div class="flex items-center gap-2 mt-6">
-                <a href="/" class="flex items-center space-x-2">
-                    <YiCai width="5" height="5"/>
-                    <span class="font-semibold text-xl sm:inline-block">溢彩画高手|鸣潮</span>
-                </a>
-            </div>
-            <Sidebar.Menu>
-                {#each commonItems as item(item.title)}
-                    <Sidebar.MenuItem>
-                        <Sidebar.MenuButton isActive={isPathActive(item.url)}>
-                            {#snippet child({props})}
-                            <a
-                                    href={item.url}
-                                    {...props}
-                                    on:click={handleClick}
-                            >
-                                <item.icon class="h-4 w-4"/>
-                                <span>{item.title}</span>
-                            </a>
-                            {/snippet}
-                        </Sidebar.MenuButton>
-                    </Sidebar.MenuItem>
-                {/each}
-            </Sidebar.Menu>
-        </Sidebar.Header>
-        <Separator class="my-4"/>
-        <Sidebar.GroupLabel class="px-3 text-sm font-semibold">
-            题目列表
-        </Sidebar.GroupLabel>
-        <Sidebar.Content class="p-1">
-            <Sidebar.Group>
 
-                <Sidebar.GroupContent>
-                    <Sidebar.Menu>
+<Sidebar.Root class="border-r" collapsible="offcanvas" side="left" variant="sidebar">
+    <Sidebar.Header class="px-4 py-2">
+        <div class="flex items-center gap-2 mt-6">
+            <a class="flex items-center space-x-2" href="/">
+                <YiCai height="5" width="5"/>
+                <span class="font-semibold text-xl sm:inline-block">溢彩画高手|鸣潮</span>
+            </a>
+        </div>
+        <Sidebar.Menu>
+            {#each commonItems as item(item.title)}
+                <Sidebar.MenuItem>
+                    <Sidebar.MenuButton isActive={isPathActive(item.url)}>
+                        {#snippet child({props})}
+                        <a
+                                href={item.url}
+                                {...props}
+                                on:click={handleClick}
+                        >
+                            <item.icon class="h-4 w-4"/>
+                            <span>{item.title}</span>
+                        </a>
+                        {/snippet}
+                    </Sidebar.MenuButton>
+                </Sidebar.MenuItem>
+            {/each}
+        </Sidebar.Menu>
+    </Sidebar.Header>
+    <Separator class="my-4"/>
+    <Sidebar.GroupLabel class="px-3 text-sm font-semibold">
+        题目列表
+    </Sidebar.GroupLabel>
+    <Sidebar.Content class="p-1">
+        <Sidebar.Group>
+            <Sidebar.GroupContent>
+                <Sidebar.Menu>
+                    {#if loadingSidebar}
+                        <LoadingSpinner text="尽力拉取题目列表..."/>
+                    {:else}
                         {#each puzzles as puzzle (puzzle.id)}
                             <Sidebar.MenuItem>
                                 <Sidebar.MenuButton isActive={isPathActive(`/puzzles/${puzzle.id}`)}>
@@ -121,21 +121,21 @@
                                 </Sidebar.MenuButton>
                             </Sidebar.MenuItem>
                         {/each}
-                    </Sidebar.Menu>
-                </Sidebar.GroupContent>
-            </Sidebar.Group>
-        </Sidebar.Content>
+                    {/if}
+                </Sidebar.Menu>
+            </Sidebar.GroupContent>
+        </Sidebar.Group>
+    </Sidebar.Content>
 
-        <Sidebar.Footer class="border-t p-4">
-            <Button
-                    variant="outline"
-                    class="w-full justify-start gap-2"
-                    href="https://github.com/gantrol/yicai_wuthering_waves/issues"
-                    target="_blank"
-            >
-                <Github class="h-4 w-4"/>
-                <span>报问题？点这里</span>
-            </Button>
-        </Sidebar.Footer>
-    </Sidebar.Root>
-{/if}
+    <Sidebar.Footer class="border-t p-4">
+        <Button
+                class="w-full justify-start gap-2"
+                href="https://github.com/gantrol/yicai_wuthering_waves/issues"
+                target="_blank"
+                variant="outline"
+        >
+            <Github class="h-4 w-4"/>
+            <span>报问题？点这里</span>
+        </Button>
+    </Sidebar.Footer>
+</Sidebar.Root>
