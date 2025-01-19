ed312c0047e931e614f2a11619ae866eedf1ae44
gantrol <501963347@qq.com>
2025-01-19 22:02:40 +0800

    i18n: revert change of layout

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,39 +1,14 @@
-import { locales, loadTranslations, translations, defaultLocale, addTranslations, setLocale, setRoute } from '$lib/translations';
-
-function pickLocal(locale_cookie, locale_al) {
-    let locale;
-    if (locale_cookie) {
-        locale = locale_cookie;
-    } else if (locale_al) {
-        locale = locale_al
-    } else if (navigator.language) {
-        locale = navigator.language.toLowerCase();
-    }
-
-    const supportedLocales = locales.get().map((l) => l.toLowerCase());
-    if (!supportedLocales.includes(locale)) {
-        locale = defaultLocale;
-    }
-
-    return locale;
-}
+import { addTranslations, setLocale, setRoute } from '$lib/translations';
 
 /** @type {import('@sveltejs/kit').Load} */
 export const load = async ({ data }) => {
-    const {
-        route,
-        locale_cookie,
-        locale_al,
-    } = data;
+    const { i18n, translations } = data;
+    const { locale, route } = i18n;
 
-
-    const locale = pickLocal(locale_cookie, locale_al)
-
-    await loadTranslations(locale, route);
-    addTranslations(translations.get());
+    addTranslations(translations);
 
     await setRoute(route);
     await setLocale(locale);
 
-    return  { locale, route } ;
+    return i18n;
 };
